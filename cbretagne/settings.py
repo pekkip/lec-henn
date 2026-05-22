623e7715d654611bdb876c7564e9b4fb9824307b
pekkip <d.pekkip@compagnonsbatisseurs.eu>
2026-05-23 00:35:53 +0200

    Config Railway 3

--- cbretagne/settings.py
+++ cbretagne/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i__ygeb%x7vyjq1%oyn1#=r_yq#tlr@^$4cj-ty*403@r!6(gv'
+SECRET_KEY =  os.environ.get('SECRET_KEY', 'django-insecure-i__ygeb%x7vyjq1%oyn1#=r_yq#tlr@^$4cj-ty*403@r!6(gv')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
 
 
 # Application definition
