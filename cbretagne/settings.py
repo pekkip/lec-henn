42a2cbcef930ebefe25716beed2b93eaad360b13
pekkip <d.pekkip@compagnonsbatisseurs.eu>
2026-05-23 23:25:07 +0200

    PostgreSQL Railway

--- cbretagne/settings.py
+++ cbretagne/settings.py
@@ -78,13 +78,17 @@ WSGI_APPLICATION = 'cbretagne.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES = {'default': dj_database_url.parse(DATABASE_URL)}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
