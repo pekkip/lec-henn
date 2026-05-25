0ff55c7a9dbc645a05f961826a317fb40dfc7ace
pekkip <d.pekkip@compagnonsbatisseurs.eu>
2026-05-25 22:58:43 +0200

    fix: migrate avant gunicorn dans Procfile

--- cbretagne/settings.py
+++ cbretagne/settings.py
@@ -129,3 +129,29 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
